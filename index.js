3dafbbcbf2008c7ee43a3e2ee4a006312f54ca71
YochaiSabag <144121949+YochaiSabag@users.noreply.github.com>
2026-02-04 22:22:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import pkg from "pg";
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url";8
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);

66688ccc2a2cfedc6964f33e975b3f3b454dee5b
YochaiSabag <yochai121@gmail.com>
2026-05-26 21:11:01 +0300

    עדכון סקרייפרים

--- test_scrapers.js
+++ test_scrapers.js
@@ -34,6 +34,9 @@ const SCRAPERS = {
   'st-fashion': './scrapers/st_fashion_scraper.js',
   'salina':      './scrapers/salina_scraper.js',
   'myme':         './scrapers/myme_scraper.js',
+  'shebello':    './scrapers/shebello_scraper.js',
+  'moda':        './scrapers/moda_scraper.js',
+  'europisrael': './scrapers/europisrael_scraper.js',
 };
 
 const args = process.argv.slice(2).map(a => a.toLowerCase());
